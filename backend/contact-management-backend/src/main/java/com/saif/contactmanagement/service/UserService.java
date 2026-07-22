232072cf1be7a663e6cdfba53eb7490f56734f56
iamSaifulhassan <saifulhassan706@gmail.com>
2026-07-23 02:00:58 +0500

    feat: implement Contact and User service methods and add validation rules (next I need to work on controllers layer)

--- backend/contact-management-backend/src/main/java/com/saif/contactmanagement/service/UserService.java
+++ backend/contact-management-backend/src/main/java/com/saif/contactmanagement/service/UserService.java
@@ -1,4 +1,12 @@
 package com.saif.contactmanagement.service;
 
+import com.saif.contactmanagement.entity.User;
+
 public interface UserService {
+
+    User register(User user);
+
+    User login(String email, String password);
+
+    void changePassword(Long userId, String oldPassword, String newPassword);
 }
--- backend/contact-management-backend/src/main/java/com/saif/contactmanagement/service/impl/UserServiceImpl.java
+++ backend/contact-management-backend/src/main/java/com/saif/contactmanagement/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.saif.contactmanagement.service.impl;
 
+import com.saif.contactmanagement.entity.User;
 import com.saif.contactmanagement.repository.UserRepository;
 import com.saif.contactmanagement.service.UserService;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,23 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
+    //constructor injection
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+    //register method implementation
+    @Override
+public User register(User user) {
+    return userRepository.save(user);
+}
+    //login method implementation
+    @Override
+    public User login(String email, String password) {
+        return null;
+    }
+    //change password method implementation
+    @Override
+    public void changePassword(Long userId, String oldPassword, String newPassword) {
+
     }
 }
