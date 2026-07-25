0a8616c69b45ad0cc13a882dc73e4e3ea21db529
iamSaifulhassan <saifulhassan706@gmail.com>
2026-07-26 01:43:47 +0500

    feat: implement user registration with validation and global exception handling

--- backend/contact-management-backend/src/main/java/com/saif/contactmanagement/config/SecurityConfig.java
+++ backend/contact-management-backend/src/main/java/com/saif/contactmanagement/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.saif.contactmanagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +14,16 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
 }
